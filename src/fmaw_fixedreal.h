24d0fcf8b365d10a2dee17d87a9907a227d0a97b
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2015-02-15 11:25:20 +0100

    + Moved FixedReal class to FMAW namespace. + Added a LOT of TODOs. + Renamed class to "NewFixedReal", "New" prefix must be removed once it can replace previous declaration of FixedReal (typedef int FixedReal). + Style changes to pass Clang Linter.

--- src/fmaw_fixedreal.h
+++ src/fmaw_fixedreal.h
@@ -1,39 +1,66 @@
 #ifndef FMAW_FIXEDREAL_H
 #define FMAW_FIXEDREAL_H
 
-class fixedreal {
+namespace FMAW {
+
+// TODO: Remove "New" prefix from class name once it can substitute Framework's
+//       previous definition of FixedReal (int).
+class NewFixedReal {
 private:
-  int num;
-  short int fraction_size;
+    int num;
+    short int fraction_size;
 public:
-  fixedreal(double d, short int fs); // fixedreal (32-fs).fs with value d
-  //fixedreal(double d);  // fixedreal 24.8 with value d
-  fixedreal(int n, short int fs); // fixedreal (32-fs).fs with value (n).0
-  //fixedreal(int n); // fixedreal 24.8 with value (n).0
-  fixedreal(short int fs);  // fixedreal (32-fs).fs with value 0.0
-  fixedreal();      // fixedreal 24.8 with value 0.0
-  
-  fixedreal& operator+=(const fixedreal x); // add x to self
-  fixedreal& operator-=(const fixedreal x); // substract x from self
-  /*
-  fixedreal& operator*=(const fixedreal x); // multiply self by x
-  fixedreal& operator/=(const fixedreal x); // divide self by x
-  */
-  
-  int raw() const;  // return the fixedreal number as is
-  /*
-  int toInt() const;  // return the integer part of the fixedreal number
-  double toDouble() const;  // return the fixedreal number as a double
-  */
-  
-  /*
-  bool operator==(const self &x, const self &y);
-  bool operator!=(const self &x, const self &y);
-  bool operator>(const self &x, const self &y);
-  bool operator<(const self &x, const self &y);
-  bool operator>=(const self &x, const self &y);
-  bool operator<=(const self &x, const self &y);
-  */
+    // TODO: Document me properly!
+    NewFixedReal(double d, short int fs); // NewFixedReal (32-fs).fs with value d
+    //NewFixedReal(double d);  // NewFixedReal 24.8 with value d
+    // TODO: Document me properly!
+    NewFixedReal(int n, short int fs); // NewFixedReal (32-fs).fs with value (n).0
+    //NewFixedReal(int n); // NewFixedReal 24.8 with value (n).0
+    // TODO: Document me properly!
+    NewFixedReal(short int fs);  // NewFixedReal (32-fs).fs with value 0.0
+    // TODO: Document me properly!
+    NewFixedReal();      // NewFixedReal 24.8 with value 0.0
+
+    // TODO: Document me properly!
+    NewFixedReal &operator+=(const NewFixedReal x); // add x to self
+    // TODO: Add support for + operator (not just += operator).
+    // TODO: Document me properly!
+    NewFixedReal &operator-=(const NewFixedReal x); // substract x from self
+    // TODO: Add support for - operator (not just -= operator).
+    /*
+    // TODO: Document me properly!
+    NewFixedReal& operator*=(const NewFixedReal x); // multiply self by x
+    // TODO: Add support for * operator (not just *= operator).
+    // TODO: Document me properly!
+    NewFixedReal& operator/=(const NewFixedReal x); // divide self by x
+    // TODO: Add support for / operator (not just /= operator).
+    */
+
+    // TODO: Document me properly!
+    int raw() const;  // return the NewFixedReal number as is
+    /*
+    // TODO: Document me properly!
+    int toInt() const;  // return the integer part of the NewFixedReal number
+    // TODO: Document me properly!
+    double toDouble() const;  // return the NewFixedReal number as a double
+    */
+
+    /*
+    // TODO: Document me properly!
+    bool operator==(const self &x, const self &y);
+    // TODO: Document me properly!
+    bool operator!=(const self &x, const self &y);
+    // TODO: Document me properly!
+    bool operator>(const self &x, const self &y);
+    // TODO: Document me properly!
+    bool operator<(const self &x, const self &y);
+    // TODO: Document me properly!
+    bool operator>=(const self &x, const self &y);
+    // TODO: Document me properly!
+    bool operator<=(const self &x, const self &y);
+    */
 };
+
+}
 
 #endif
