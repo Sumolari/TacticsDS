2b9194c8975df00899214b2e0bffd49aa543cd49
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2015-02-18 19:08:21 +0100

    Created aliases to work easily with files

--- src/fmaw_character.cpp
+++ src/fmaw_character.cpp
@@ -1,27 +1,7 @@
 // Copyright 2015 Lluís Ulzurrun de Asanza Sàez
 
-#include "./fmaw_character.h"
-
-#include "./FMAW.h"
-
-namespace FMAW {
-
-void Character::render(int camera_x, int camera_y) {
-    FixedReal x, y;
-    x = ((this->x - this->width) >> 8) - camera_x;
-    y = ((this->y - this->height) >> 8) - camera_y;
-
-    if (x <= -16 || y <= -16 || x >= 256 || y >= 192) {
-        this->sprite.disable();
-        return;
-    }
-
-    this->sprite.setPosition(x, y);
-}
-
-void Character::print() {
-    FMAW::printf("Character using sprite:");
-    this->sprite.print();
-}
-
-}  // namespace FMAW
+#ifdef NDS
+#include "./fmaw_character.fds"
+#elif OPENGL
+#include "./fmaw_character.fgl"
+#endif
