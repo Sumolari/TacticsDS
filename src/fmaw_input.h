bbc2ef5f8f2fa109fa6aab6e20ab7f30db4de48a
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2015-02-19 10:19:46 +0100

    Added whilePressed methods

--- src/fmaw_input.h
+++ src/fmaw_input.h
@@ -20,14 +20,21 @@ typedef enum BUTTONS ButtonType;
  * @param callback  Function to be called when button A is pressed.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonAPressed(void (*callback));
+int onButtonAPressed(void (*callback)());
+
+/**
+ * Registers a callback so it will be called while button A is pressed.
+ * @param callback  Function to be called while button A is pressed.
+ * @return Identifier of the callback so it can be registered later.
+ */
+int whileButtonAPressed(void (*callback)());
 
 /**
  * Registers a callback so it will be called when button A is released.
  * @param callback  Function to be called when button A is released.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonAReleased(void (*callback));
+int onButtonAReleased(void (*callback)());
 
 //------------------------------------------------------------------------------
 // Handling Button B.
@@ -38,14 +45,21 @@ int onButtonAReleased(void (*callback));
  * @param callback  Function to be called when button B is pressed.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonBPressed(void (*callback));
+int onButtonBPressed(void (*callback)());
+
+/**
+ * Registers a callback so it will be called while button B is pressed.
+ * @param callback  Function to be called while button B is pressed.
+ * @return Identifier of the callback so it can be registered later.
+ */
+int whileButtonBPressed(void (*callback)());
 
 /**
  * Registers a callback so it will be called when button B is released.
  * @param callback  Function to be called when button B is released.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonBReleased(void (*callback));
+int onButtonBReleased(void (*callback)());
 
 //------------------------------------------------------------------------------
 // Handling Button L.
@@ -56,14 +70,21 @@ int onButtonBReleased(void (*callback));
  * @param callback  Function to be called when button L is pressed.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonLPressed(void (*callback));
+int onButtonLPressed(void (*callback)());
+
+/**
+ * Registers a callback so it will be called while button L is pressed.
+ * @param callback  Function to be called while button L is pressed.
+ * @return Identifier of the callback so it can be registered later.
+ */
+int whileButtonLPressed(void (*callback)());
 
 /**
  * Registers a callback so it will be called when button L is released.
  * @param callback  Function to be called when button L is released.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonLReleased(void (*callback));
+int onButtonLReleased(void (*callback)());
 
 //------------------------------------------------------------------------------
 // Handling Button R.
@@ -74,14 +95,21 @@ int onButtonLReleased(void (*callback));
  * @param callback  Function to be called when button R is pressed.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonRPressed(void (*callback));
+int onButtonRPressed(void (*callback)());
+
+/**
+ * Registers a callback so it will be called while button R is pressed.
+ * @param callback  Function to be called while button R is pressed.
+ * @return Identifier of the callback so it can be registered later.
+ */
+int whileButtonRPressed(void (*callback)());
 
 /**
  * Registers a callback so it will be called when button R is released.
  * @param callback  Function to be called when button R is released.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonRReleased(void (*callback));
+int onButtonRReleased(void (*callback)());
 
 //------------------------------------------------------------------------------
 // Handling Button Select.
@@ -92,14 +120,21 @@ int onButtonRReleased(void (*callback));
  * @param callback  Function to be called when button Select is pressed.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonSelectPressed(void (*callback));
+int onButtonSelectPressed(void (*callback)());
+
+/**
+ * Registers a callback so it will be called while button Select is pressed.
+ * @param callback  Function to be called while button Select is pressed.
+ * @return Identifier of the callback so it can be registered later.
+ */
+int whileButtonSelectPressed(void (*callback)());
 
 /**
  * Registers a callback so it will be called when button Select is released.
  * @param callback  Function to be called when button Select is released.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonSelectReleased(void (*callback));
+int onButtonSelectReleased(void (*callback)());
 
 //------------------------------------------------------------------------------
 // Handling Button Start.
@@ -110,14 +145,21 @@ int onButtonSelectReleased(void (*callback));
  * @param callback  Function to be called when button Start is pressed.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonStartPressed(void (*callback));
+int onButtonStartPressed(void (*callback)());
+
+/**
+ * Registers a callback so it will be called while button Start is pressed.
+ * @param callback  Function to be called while button Start is pressed.
+ * @return Identifier of the callback so it can be registered later.
+ */
+int whileButtonStartPressed(void (*callback)());
 
 /**
  * Registers a callback so it will be called when button Start is released.
  * @param callback  Function to be called when button Start is released.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonStartReleased(void (*callback));
+int onButtonStartReleased(void (*callback)());
 
 //------------------------------------------------------------------------------
 // Handling Button Arrow Up.
@@ -128,14 +170,21 @@ int onButtonStartReleased(void (*callback));
  * @param callback  Function to be called when button ArrowUp is pressed.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonArrowUpPressed(void (*callback));
+int onButtonArrowUpPressed(void (*callback)());
+
+/**
+ * Registers a callback so it will be called while button ArrowUp is pressed.
+ * @param callback  Function to be called while button ArrowUp is pressed.
+ * @return Identifier of the callback so it can be registered later.
+ */
+int whileButtonArrowUpPressed(void (*callback)());
 
 /**
  * Registers a callback so it will be called when button ArrowUp is released.
  * @param callback  Function to be called when button ArrowUp is released.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonArrowUpReleased(void (*callback));
+int onButtonArrowUpReleased(void (*callback)());
 
 //------------------------------------------------------------------------------
 // Handling Button Arrow Down.
@@ -146,14 +195,21 @@ int onButtonArrowUpReleased(void (*callback));
  * @param callback  Function to be called when button ArrowDown is pressed.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonArrowDownPressed(void (*callback));
+int onButtonArrowDownPressed(void (*callback)());
+
+/**
+ * Registers a callback so it will be called while button ArrowDown is pressed.
+ * @param callback  Function to be called while button ArrowDown is pressed.
+ * @return Identifier of the callback so it can be registered later.
+ */
+int whileButtonArrowDownPressed(void (*callback)());
 
 /**
  * Registers a callback so it will be called when button ArrowDown is released.
  * @param callback  Function to be called when button ArrowDown is released.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonArrowDownReleased(void (*callback));
+int onButtonArrowDownReleased(void (*callback)());
 
 //------------------------------------------------------------------------------
 // Handling Button Arrow Left.
@@ -164,14 +220,21 @@ int onButtonArrowDownReleased(void (*callback));
  * @param callback  Function to be called when button ArrowLeft is pressed.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonArrowLeftPressed(void (*callback));
+int onButtonArrowLeftPressed(void (*callback)());
+
+/**
+ * Registers a callback so it will be called while button ArrowLeft is pressed.
+ * @param callback  Function to be called while button ArrowLeft is pressed.
+ * @return Identifier of the callback so it can be registered later.
+ */
+int whileButtonArrowLeftPressed(void (*callback)());
 
 /**
  * Registers a callback so it will be called when button ArrowLeft is released.
  * @param callback  Function to be called when button ArrowLeft is released.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonArrowLeftReleased(void (*callback));
+int onButtonArrowLeftReleased(void (*callback)());
 
 //------------------------------------------------------------------------------
 // Handling Button Arrow Right.
@@ -182,14 +245,21 @@ int onButtonArrowLeftReleased(void (*callback));
  * @param callback  Function to be called when button ArrowRight is pressed.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonArrowRightPressed(void (*callback));
+int onButtonArrowRightPressed(void (*callback)());
+
+/**
+ * Registers a callback so it will be called while button ArrowRight is pressed.
+ * @param callback  Function to be called while button ArrowRight is pressed.
+ * @return Identifier of the callback so it can be registered later.
+ */
+int whileButtonArrowRightPressed(void (*callback)());
 
 /**
  * Registers a callback so it will be called when button ArrowRight is released.
  * @param callback  Function to be called when button ArrowRight is released.
  * @return Identifier of the callback so it can be registered later.
  */
-int onButtonArrowRightReleased(void (*callback));
+int onButtonArrowRightReleased(void (*callback)());
 
 //------------------------------------------------------------------------------
 // Callback handling.
