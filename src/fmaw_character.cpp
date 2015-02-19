a08e767b330c1991a40ce8df589613cef1d0fbc8
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2015-02-19 17:46:04 +0100

    Step by step merge #3

--- src/fmaw_character.cpp
+++ src/fmaw_character.cpp
@@ -1,28 +1,7 @@
 // Copyright 2015 FMAW
 
-#include "./fmaw_character.h"
-
-#include "./FMAW.h"
-
-namespace FMAW {
-
-void Character::render(FixedReal camera_x, FixedReal camera_y) {
-    FixedReal x, y;
-    x = (this->x - FixedReal(this->width, 8)) - camera_x;
-    y = (this->y - FixedReal(this->height, 8)) - camera_y;
-
-    if (x <= FixedReal(-16, 8) || y <= FixedReal(-16, 8) ||
-            x >= FixedReal(256, 8) || y >= FixedReal(192, 8)) {
-        this->sprite.disable();
-        return;
-    }
-
-    this->sprite.setPosition(x, y);
-}
-
-void Character::print() {
-    FMAW::printf("Character using sprite:");
-    this->sprite.print();
-}
-
-}  // namespace FMAW
+#ifdef NDS
+#include "./fmaw_character.fds"
+#elif OPENGL
+#include "./fmaw_character.fgl"
+#endif
---
+++ src/fmaw_geometry.cpp
@@ -0,0 +1,7 @@
+// Copyright 2015 Lluís Ulzurrun de Asanza Sàez
+
+#ifdef NDS
+#include "./fmaw_geometry.fds"
+#elif OPENGL
+#include "./fmaw_geometry.fgl"
+#endif
