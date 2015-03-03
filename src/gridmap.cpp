b221e2564ba5b8143ad3651e08700c970baf42e0
Victor <vickto.gr@gmail.com>
2015-03-03 10:12:59 +0100

    ! gridmap is now a namespace, not a class.\n! Changed defaultMap to use Cell identifiers for each background cell type.\n- Deleted gridmap.h, since gridmap isn't a class anymore.

--- src/gridmap.cpp
+++ src/gridmap.cpp
@@ -1,37 +1,30 @@
 // Copyright 2015 FMAW
 
-#include <vector>
-#include <stdexcept>
 #include <iostream>
 #include <fstream>
-#include "./gridmap.h"
+#include "./grid.h"
 
+namespace GridMap{
 
-//GridMap::GridMap() : GridMap("defaultMap") {}
-GridMap::GridMap(const char* mapName){
+void loadDefaultGridMap( Grid g ) { loadGridMap("defaultMap", g); }
+
+void loadGridMap( const char* mapName, Grid &g ){
 	std::ifstream mapFile;
 	mapFile.open( mapName );
 	
-	mapFile >> this->height >> this->width;
-	this->mapMat = std::vector< std::vector<int> >(
-		this->height, std::vector<int>(this->width)
-	);
-	for( int i = 0; i < this->height; i++ ){
-		for( int j = 0; j < this->width; j++ ){
-			mapFile >> this->mapMat[i][j];
+	int rows, cols, aux;
+	
+	mapFile >> rows >> cols;
+	for( int row = 0; i < rows; i++ ){
+		for( int col = 0; j < cols; j++ ){
+			mapFile >> aux;
+			
+			IndexPath path {row, col};
+			g.cellAtIndexPath(path)->setBackgroundType( aux );
 		}
 	}
 	
 	mapFile.close();
 }
 
-int GridMap::getPosition(int x, int y){
-	if( x >= this->height || y >= this->width || x < 0 || y < 0 )
-	    throw std::out_of_range("The requested position is "
-								"out of the map boundaries.");
-	
-	return this->mapMat[x][y];
 }
-
-int GridMap::rows(){ return this->height; }
-int GridMap::cols(){ return this->width; }
